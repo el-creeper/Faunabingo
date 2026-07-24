d6191eff36c15af60584eb87eb34d69a57cfbcc6
Elliot P <elliotpicavet@me.com>
2026-07-24 03:08:59 +0200

    Modification pour charger les images

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# 1. On copie les photos initiales vers le disque dur de sauvegarde
-cp -rn static/images/. /app/data/ 2>/dev/null || true
+# 1. On prépare les vrais dossiers dans le disque dur de sauvegarde
+mkdir -p /app/data/database
+mkdir -p /app/data/static
 
-# 2. On supprime les dossiers locaux et on crée les ponts vers la sauvegarde
-rm -rf database static/images
-mkdir -p static
-ln -s /app/data database
-ln -s /app/data static/images
+# 2. On copie toutes les images et fichiers de style vers la sauvegarde
+cp -rn static/* /app/data/static/ 2>/dev/null || true
 
-# 3. On initialise la base de données
+# 3. On supprime les dossiers d'origine
+rm -rf database
+rm -rf static
+
+# 4. On crée des raccourcis globaux
+ln -s /app/data/database database
+ln -s /app/data/static static
+
+# 5. On initialise la base de données et on lance le serveur
 python init_db.py
-
-# 4. On lance l'application
 uvicorn main:app --host 0.0.0.0 --port $PORT
