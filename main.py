7a6ebc929c10a8cad51aff480128addfbfbf120f
Elliot P <elliotpicavet@me.com>
2026-08-16 21:02:44 +0200

    Ajout de l'export sécurisé par variable d'environnement

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import sqlite3
 import uuid
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from routers import admin, jeu
+from fastapi.responses import FileResponse
+import os
+
+
+
+
 
 app = FastAPI(title="FaunaBingo")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 DB_NAME = "database/bingo_faune.db"
+
+@app.get("/export-bdd")
+def export_base_de_donnees(cle_secrete: str = ""):
+    # Le code demande le mot de passe aux paramètres cachés du serveur
+    VRAI_MOT_DE_PASSE = os.getenv("EXPORT_SECRET")
+    
+    # Sécurité : Si le mot de passe n'est pas configuré sur le serveur, ou s'il est faux, on bloque
+    if not VRAI_MOT_DE_PASSE or cle_secrete != VRAI_MOT_DE_PASSE:
+        raise HTTPException(status_code=403, detail="Accès refusé.")
+
+    chemin_db = "database/bingo_faune.db"
+    
+    if os.path.exists(chemin_db):
+        return FileResponse(
+            path=chemin_db, 
+            filename="scores_costa_rica_final.db", 
+            media_type="application/octet-stream"
+        )
+    return {"erreur": "La base de données est introuvable."}
+
 
 def inserer_donnees_test():
     with sqlite3.connect(DB_NAME) as conn:
